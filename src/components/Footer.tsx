cce93210770eb8726fe372fc57857377ae92538f
LiasCode <liasraul00@gmail.com>
2025-03-10 03:01:10 -0400

    fix(docs): footer word spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
       <div class="container flex flex-col items-center justify-between gap-4 px-4 md:h-24 md:flex-row md:px-8">
         <div class="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p class="text-center text-muted-foreground text-sm leading-loose md:text-left">
-            Built &amp; designed by
+            Built &amp; designed by{" "}
             <a
               href="https://twitter.com/shadcn"
               target="_blank"

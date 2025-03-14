0bc82e33a2a317088e0abc7b5c773d54623773e0
LiasCode <liasraul00@gmail.com>
2025-03-14 18:03:02 -0400

    fix(docs): checkbox guide

--- src/routes/Components/Sections/CheckBoxSection.tsx
+++ src/routes/Components/Sections/CheckBoxSection.tsx
@@ -1,46 +1,154 @@
+import { CodePreviewTabs } from "@/components/CodePreview/CodePreviewTabs";
+import HighlightCode from "@/components/CodePreview/HighlightCode";
+import { AppRoutes } from "@/routes/AppRoutes";
 import { Button } from "@ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@ui/card";
 import { Checkbox } from "@ui/checkbox";
-import { useState } from "preact/hooks";
+import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
+import { ChevronLeft, ChevronRight } from "lucide-preact";
+import { A } from "preact-hashish-router";
 
 export function CheckboxSection() {
-  const [c, setC] = useState(false);
-
   return (
-    <div className="flex w-full flex-col items-start gap-10 pt-4">
+    <div className="flex w-full flex-col gap-6">
+      <CodePreviewTabs
+        codeString={`
+  import { Checkbox } from "@ui/checkbox"
+
+  export function CheckboxDemo() {
+    return (
       <div className="flex items-center space-x-2">
-        <Checkbox id="terms32" checked={c} onCheckedChange={setC} />
+        <Checkbox id="terms" />
         <label
-          htmlFor="terms32"
-          className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor="terms"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
         >
-          Accept terms and conditions. Controlled Check
+          Accept terms and conditions
         </label>
       </div>
+    )
+  }
 
+`}
+        previewElement={
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Accept terms and conditions
+            </label>
+          </div>
+        }
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Usage</h2>
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  import { Checkbox } from "@/components/ui/checkbox"
+
+`}
+      />
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  <Checkbox />
+
+`}
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Examples</h2>
+
+      <h3 className="w-full font-semibold text-lg">With Text</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Checkbox } from "@ui/checkbox"
+
+  export function CheckboxWithText() {
+    return (
       <div className="items-top flex space-x-2">
         <Checkbox id="terms1" />
         <div className="grid gap-1.5 leading-none">
           <label
             htmlFor="terms1"
-            className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
             Accept terms and conditions
           </label>
-          <p className="text-muted-foreground text-sm">You agree to our Terms of Service and Privacy Policy.</p>
+          <p className="text-sm text-muted-foreground">
+            You agree to our Terms of Service and Privacy Policy.
+          </p>
         </div>
       </div>
+    )
+  }
 
+`}
+        previewElement={
+          <div className="items-top flex space-x-2">
+            <Checkbox id="terms1" />
+            <div className="grid gap-1.5 leading-none">
+              <label
+                htmlFor="terms1"
+                className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Accept terms and conditions
+              </label>
+              <p className="text-muted-foreground text-sm">You agree to our Terms of Service and Privacy Policy.</p>
+            </div>
+          </div>
+        }
+      />
+
+      <h3 className="w-full font-semibold text-lg">Disable</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Checkbox } from "@ui/checkbox"
+
+  export function CheckboxDisabled() {
+    return (
       <div className="flex items-center space-x-2">
         <Checkbox id="terms2" disabled />
         <label
           htmlFor="terms2"
-          className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
         >
           Accept terms and conditions
         </label>
       </div>
+    )
+  }
 
+`}
+        previewElement={
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms2" disabled />
+            <label
+              htmlFor="terms2"
+              className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Accept terms and conditions
+            </label>
+          </div>
+        }
+      />
+
+      <h3 className="w-full font-semibold text-lg">Form</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Button } from "@ui/button";
+  import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@ui/card";
+  import { Checkbox } from "@ui/checkbox";
+
+  const CheckboxDemo = () => (
+    <form>
       <Card className="max-w-[350px]">
         <CardHeader>
           <CardTitle>Sidebar</CardTitle>
@@ -97,22 +205,101 @@ export function CheckboxSection() {
               Downloads
             </label>
           </div>
-
-          <div className="flex items-center space-x-2">
-            <Checkbox id="docs" />
-            <label
-              htmlFor="docs"
-              className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Documents
-            </label>
-          </div>
         </CardContent>
 
         <CardFooter>
           <Button>Submit</Button>
         </CardFooter>
       </Card>
+    </form>
+  )
+`}
+        previewElement={
+          <Card className="max-w-[350px]">
+            <CardHeader>
+              <CardTitle>Sidebar</CardTitle>
+              <CardDescription>Select the items you want to display in the sidebar.</CardDescription>
+            </CardHeader>
+
+            <CardContent className="flex flex-col gap-2">
+              <div className="flex items-center space-x-2">
+                <Checkbox id="recents" defaultChecked />
+                <label
+                  htmlFor="recents"
+                  className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Recents
+                </label>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Checkbox id="home" defaultChecked />
+                <label
+                  htmlFor="home"
+                  className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Home
+                </label>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Checkbox id="app" />
+                <label
+                  htmlFor="app"
+                  className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Applications
+                </label>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Checkbox id="desktop" />
+                <label
+                  htmlFor="desktop"
+                  className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Desktop
+                </label>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Checkbox id="downloads" />
+                <label
+                  htmlFor="downloads"
+                  className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Downloads
+                </label>
+              </div>
+            </CardContent>
+
+            <CardFooter>
+              <Button>Submit</Button>
+            </CardFooter>
+          </Card>
+        }
+      />
+
+      <Pagination className="mt-10">
+        <PaginationContent className="flex w-full flex-row justify-between">
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.CHART}>
+              <Button className="gap-1 pl-1" variant="outline">
+                <ChevronLeft />
+                Chart
+              </Button>
+            </A>
+          </PaginationItem>
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.COMMAND}>
+              <Button className="gap-1 pr-1 capitalize" variant="outline">
+                Command
+                <ChevronRight />
+              </Button>
+            </A>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }

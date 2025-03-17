50fe6705f3b2acc2d18f715a5c7523a8703e36cf
LiasCode <liasraul00@gmail.com>
2025-03-17 17:13:45 -0400

    feat(docs): drawer and input guide

--- src/routes/Components/Sections/DrawerSection.tsx
+++ src/routes/Components/Sections/DrawerSection.tsx
@@ -1,3 +1,6 @@
+import { CodePreviewTabs } from "@/components/CodePreview/CodePreviewTabs";
+import HighlightCode from "@/components/CodePreview/HighlightCode";
+import { AppRoutes } from "@/routes/AppRoutes";
 import { Button } from "@ui/button";
 import {
   Drawer,
@@ -9,37 +12,89 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@ui/drawer";
+import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
+import { ChevronLeft, ChevronRight } from "lucide-preact";
 import { Minus, Plus } from "lucide-preact";
+import { A } from "preact-hashish-router";
+import type { CSSProperties } from "preact/compat";
 import { useState } from "preact/hooks";
+import { Bar, BarChart, ResponsiveContainer } from "recharts";
 
 export default function DrawerSection() {
-  const [goal, setGoal] = useState(350);
-
-  function onClick(adjustment: number) {
-    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
-  }
-
   return (
-    <div className="flex w-full flex-col gap-10">
-      <p class="leading-7 [&amp;:not(:first-child)]:mt-6">
-        Drawer is built on top of{" "}
-        <a class="font-medium underline underline-offset-4" href="https://github.com/emilkowalski/vaul">
-          Vaul
-        </a>{" "}
-        by{" "}
-        <a class="font-medium underline underline-offset-4" href="https://twitter.com/emilkowalski_">
-          emilkowalski_
-        </a>
-        .
-      </p>
+    <div className="flex w-full flex-col gap-6">
+      <CodePreviewTabs
+        codeString={`
+  import { Minus, Plus } from "lucide-preact"
+  import { Bar, BarChart, ResponsiveContainer } from "recharts"
+  import { Button } from "@ui/button"
+  import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@ui/drawer"
+  import type { CSSProperties } from "preact/compat";
 
+
+  const data = [
+    {
+      goal: 400,
+    },
+    {
+      goal: 300,
+    },
+    {
+      goal: 200,
+    },
+    {
+      goal: 300,
+    },
+    {
+      goal: 200,
+    },
+    {
+      goal: 278,
+    },
+    {
+      goal: 189,
+    },
+    {
+      goal: 239,
+    },
+    {
+      goal: 300,
+    },
+    {
+      goal: 200,
+    },
+    {
+      goal: 278,
+    },
+    {
+      goal: 189,
+    },
+    {
+      goal: 349,
+    },
+  ];
+
+  export function DrawerDemo() {
+    const [goal, setGoal] = useState(350);
+
+    function onClick(adjustment: number) {
+      setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+    }
+
+    return (
       <Drawer>
         <DrawerTrigger asChild>
-          <Button variant="outline" className="w-fit">
-            Open Drawer
-          </Button>
+          <Button variant="outline">Open Drawer</Button>
         </DrawerTrigger>
-
         <DrawerContent>
           <div className="mx-auto w-full max-w-sm">
             <DrawerHeader>
@@ -73,6 +128,21 @@ export default function DrawerSection() {
                   <span className="sr-only">Increase</span>
                 </Button>
               </div>
+              <div className="mt-3 h-[120px]">
+                <ResponsiveContainer width="100%" height="100%">
+                  <BarChart data={data}>
+                    <Bar
+                      dataKey="goal"
+                      style={
+                        {
+                          fill: "hsl(var(--foreground))",
+                          opacity: 0.9,
+                        } as CSSProperties
+                      }
+                    />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
             </div>
             <DrawerFooter>
               <Button>Submit</Button>
@@ -83,6 +153,202 @@ export default function DrawerSection() {
           </div>
         </DrawerContent>
       </Drawer>
+    );
+  }
+
+`}
+        previewElement={
+          <div className="flex items-center space-x-2">
+            <DrawerDemo />
+          </div>
+        }
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">About</h2>
+
+      <p class="leading-[1.65rem]">
+        Drawer is built on top of{" "}
+        <a class="font-medium underline underline-offset-4" href="https://github.com/emilkowalski/vaul">
+          Vaul
+        </a>{" "}
+        by{" "}
+        <a class="font-medium underline underline-offset-4" href="https://twitter.com/emilkowalski_">
+          emilkowalski_
+        </a>
+        .
+      </p>
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Usage</h2>
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@ui/dialog"
+
+`}
+      />
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  <Dialog>
+    <DialogTrigger>Open</DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Are you absolutely sure?</DialogTitle>
+        <DialogDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+
+`}
+      />
+
+      <Pagination className="mt-10">
+        <PaginationContent className="flex w-full flex-row justify-between">
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.DIALOG}>
+              <Button className="gap-1 pl-1" variant="outline">
+                <ChevronLeft />
+                Dialog
+              </Button>
+            </A>
+          </PaginationItem>
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.INPUT}>
+              <Button className="gap-1 pr-1 capitalize" variant="outline">
+                Input
+                <ChevronRight />
+              </Button>
+            </A>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
+  );
+}
+
+const data = [
+  {
+    goal: 400,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 239,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 349,
+  },
+];
+
+export function DrawerDemo() {
+  const [goal, setGoal] = useState(350);
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="outline">Open Drawer</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+            <div className="flex items-center justify-center space-x-2">
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => onClick(-10)}
+                disabled={goal <= 200}
+              >
+                <Minus />
+                <span className="sr-only">Decrease</span>
+              </Button>
+              <div className="flex-1 text-center">
+                <div className="font-bold text-7xl tracking-tighter">{goal}</div>
+                <div className="text-[0.70rem] text-muted-foreground uppercase">Calories/day</div>
+              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => onClick(10)}
+                disabled={goal >= 400}
+              >
+                <Plus />
+                <span className="sr-only">Increase</span>
+              </Button>
+            </div>
+            <div className="mt-3 h-[120px]">
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart data={data}>
+                  <Bar
+                    dataKey="goal"
+                    style={
+                      {
+                        fill: "hsl(var(--foreground))",
+                        opacity: 0.9,
+                      } as CSSProperties
+                    }
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 }
--- src/routes/Components/Sections/InputsSection.tsx
+++ src/routes/Components/Sections/InputsSection.tsx
@@ -1,47 +1,262 @@
-import { TokenInput } from "@ui/TokenInput";
+import { CodePreviewTabs } from "@/components/CodePreview/CodePreviewTabs";
+import HighlightCode from "@/components/CodePreview/HighlightCode";
+import { AppRoutes } from "@/routes/AppRoutes";
 import { Button } from "@ui/button";
 import { Input } from "@ui/input";
-import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "@ui/input-otp";
 import { Label } from "@ui/label";
+import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
+import { toast } from "@ui/toast";
+import { ChevronLeft, ChevronRight } from "lucide-preact";
+import { A } from "preact-hashish-router";
+import { useState } from "preact/hooks";
 
 export function InputsSection() {
   return (
-    <div className="flex w-full flex-col items-center gap-10">
-      <Input type="email1" placeholder="Email" className="max-w-sm" />
+    <div className="flex w-full flex-col gap-6">
+      <CodePreviewTabs
+        codeString={`
+  import { Input } from "@ui/input"
 
+  export function InputDemo() {
+    return <Input type="email" placeholder="Email" />
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <Input type="email" placeholder="Email" />
+          </div>
+        }
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Usage</h2>
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  import { Input } from "@ui/input"
+
+`}
+      />
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  <Input />
+
+`}
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Examples</h2>
+
+      <h3 className="w-full font-semibold text-lg">Default</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Input } from "@ui/input"
+
+  export function InputDemo() {
+    return <Input type="email" placeholder="Email" />
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <Input type="email" placeholder="Email" />
+          </div>
+        }
+      />
+
+      <h3 className="w-full font-semibold text-lg">File</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Input } from "@ui/input"
+  import { Label } from "@ui/label"
+   
+  export function InputFile() {
+    return (
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="picture">Picture</Label>
         <Input id="picture" type="file" />
       </div>
+    )
+  }
 
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="picture">Picture</Label>
+              <Input id="picture" type="file" />
+            </div>
+          </div>
+        }
+      />
+
+      <h3 className="w-full font-semibold text-lg">Disabled</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Input } from "@ui/input"
+
+  export function InputDisabled() {
+    return <Input disabled type="email" placeholder="Email" />
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <Input disabled type="email" placeholder="Email" />
+          </div>
+        }
+      />
+
+      <h3 className="w-full font-semibold text-lg">With Label</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Input } from "@ui/input"
+  import { Label } from "@ui/label"
+
+  export function InputWithLabel() {
+    return (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </div>
+    )
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" placeholder="Email" />
+            </div>
+          </div>
+        }
+      />
+
+      <h3 className="w-full font-semibold text-lg">With Button</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Button } from "@ui/button"
+  import { Input } from "@ui/input"
+
+  export function InputWithButton() {
+    return (
       <div className="flex w-full max-w-sm items-center space-x-2">
         <Input type="email" placeholder="Email" />
         <Button type="submit">Subscribe</Button>
       </div>
+    )
+  }
 
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="username">Username</Label>
-        <Input disabled id="username" type="text" className="max-w-sm" placeholder="Raul..." />
-      </div>
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <div className="flex w-full max-w-sm items-center space-x-2">
+              <Input type="email" placeholder="Email" />
+              <Button type="submit">Subscribe</Button>
+            </div>
+          </div>
+        }
+      />
 
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="tokens01">Input with Tokenizer</Label>
-        <TokenInput id="tokens01" variant="default" value={["Pasta", "Compra", "Fix", "Bug", "Priority"]} />
-      </div>
+      <h3 className="w-full font-semibold text-lg">Form</h3>
 
-      <InputOTP maxLength={6}>
-        <InputOTPGroup>
-          <InputOTPSlot index={0} />
-          <InputOTPSlot index={1} />
-          <InputOTPSlot index={2} />
-        </InputOTPGroup>
-        <InputOTPSeparator />
-        <InputOTPGroup>
-          <InputOTPSlot index={3} />
-          <InputOTPSlot index={4} />
-          <InputOTPSlot index={5} />
-        </InputOTPGroup>
-      </InputOTP>
+      <CodePreviewTabs
+        codeString={`
+  import { toast } from "@ui/toast"
+  import { Button } from "@ui/button"
+  import { Label } from "@ui/label";
+  import { Input } from "@ui/input"
+  import { useState } from "preact/hooks";
+
+  export function InputForm() {
+    const [data, setData] = useState({ username: "" });
+
+    function onSubmit(e: SubmitEvent) {
+      e.preventDefault();
+      toast({
+        title: "You submitted the following values:",
+        description: JSON.stringify(data, null, 2),
+      });
+    }
+
+    return (
+      <form onSubmit={onSubmit} className="w-full space-y-6">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label className="font-semibold">Username</Label>
+          <Input
+            placeholder="shadcn"
+            value={data.username}
+            onInput={(e) => setData({ username: e.currentTarget.value })}
+          />
+          <p className="text-[0.8rem] text-muted-foreground">This is your public display name.</p>
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    );
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full max-w-[320px] items-center space-x-2">
+            <InputForm />
+          </div>
+        }
+      />
+
+      <Pagination className="mt-10">
+        <PaginationContent className="flex w-full flex-row justify-between">
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.DRAWER}>
+              <Button className="gap-1 pl-1" variant="outline">
+                <ChevronLeft />
+                Drawer
+              </Button>
+            </A>
+          </PaginationItem>
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.LABEL}>
+              <Button className="gap-1 pr-1 capitalize" variant="outline">
+                Label
+                <ChevronRight />
+              </Button>
+            </A>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
+  );
+}
+export function InputForm() {
+  const [data, setData] = useState({ username: "" });
+
+  function onSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    toast({
+      title: "You submitted the following values:",
+      description: JSON.stringify(data, null, 2),
+    });
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="w-full space-y-6">
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label className="font-semibold">Username</Label>
+        <Input
+          placeholder="shadcn"
+          value={data.username}
+          onInput={(e) => setData({ username: e.currentTarget.value })}
+        />
+        <p className="text-[0.8rem] text-muted-foreground">This is your public display name.</p>
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }

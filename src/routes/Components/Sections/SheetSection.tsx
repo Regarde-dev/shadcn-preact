d1e2529dc6c163d42369b77bfe03b3481a3038a8
LiasCode <liasraul00@gmail.com>
2025-03-17 23:13:44 -0400

    feat(docs): sheet guide

--- src/routes/Components/Sections/SheetSection.tsx
+++ src/routes/Components/Sections/SheetSection.tsx
@@ -1,6 +1,10 @@
+import { CodePreviewTabs } from "@/components/CodePreview/CodePreviewTabs";
+import HighlightCode from "@/components/CodePreview/HighlightCode";
+import { AppRoutes } from "@/routes/AppRoutes";
 import { Button } from "@ui/button";
 import { Input } from "@ui/input";
 import { Label } from "@ui/label";
+import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
 import {
   Sheet,
   SheetClose,
@@ -11,50 +15,181 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@ui/sheet";
-
-const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
+import { ChevronLeft, ChevronRight } from "lucide-preact";
+import { A } from "preact-hashish-router";
 
 export function SheetSection() {
   return (
-    <div className="flex w-full flex-col items-center gap-10">
+    <div className="flex w-full flex-col gap-6">
+      <CodePreviewTabs
+        codeString={`
+  import { Button } from "@ui/button"
+  import { Input } from "@ui/input"
+  import { Label } from "@ui/label"
+  import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@ui/sheet"
+
+  export function SheetDemo() {
+    return (
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Button variant="outline">Open</Button>
         </SheetTrigger>
-
-        <SheetContent side="left">
+        <SheetContent>
           <SheetHeader>
             <SheetTitle>Edit profile</SheetTitle>
-            <SheetDescription>Make changes to your profile here. Click save when you're done.</SheetDescription>
+            <SheetDescription>
+              Make changes to your profile here. Click save when you're done.
+            </SheetDescription>
           </SheetHeader>
-
           <div className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="sheet-1-name" className="text-right">
+              <Label htmlFor="name" className="text-right">
                 Name
               </Label>
-              <Input id="sheet-1-name" value="Pedro Duarte" className="col-span-3" />
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="sheet-1-username" className="text-right">
+              <Label htmlFor="username" className="text-right">
                 Username
               </Label>
-              <Input id="sheet-1-username" value="@peduarte" className="col-span-3" />
+              <Input id="username" value="@peduarte" className="col-span-3" />
             </div>
           </div>
-
           <SheetFooter>
-            <SheetClose>
-              <Button>Save changes</Button>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
             </SheetClose>
           </SheetFooter>
         </SheetContent>
       </Sheet>
+    )
+  }
 
+`}
+        previewElement={
+          <div className="flex w-full items-center justify-center space-x-2">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline">Open</Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Edit profile</SheetTitle>
+                  <SheetDescription>Make changes to your profile here. Click save when you're done.</SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      Username
+                    </Label>
+                    <Input id="username" value="@peduarte" className="col-span-3" />
+                  </div>
+                </div>
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button type="submit">Save changes</Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
+          </div>
+        }
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Usage</h2>
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@ui/sheet"
+
+`}
+      />
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  <Sheet>
+    <SheetTrigger>Open</SheetTrigger>
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Are you absolutely sure?</SheetTitle>
+        <SheetDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </SheetDescription>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
+
+`}
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Examples</h2>
+
+      <h3 className="w-full font-semibold text-xl">Side</h3>
+
+      <p className="leading-[1.65rem]">
+        Use the <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">side</code>{" "}
+        property to{" "}
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">
+          &lt;SheetContent /&gt;
+        </code>{" "}
+        to indicate the edge of the screen where the component will appear. The values can be{" "}
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">top</code>,{" "}
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">right</code>,{" "}
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">bottom</code> or{" "}
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">left</code>.
+      </p>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Button } from "@ui/button"
+  import { Input } from "@ui/input"
+  import { Label } from "@ui/label"
+  import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@ui/sheet"
+
+  const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
+
+  type SheetSide = (typeof SHEET_SIDES)[number];
+
+  export function SheetSide() {
+    return (
       <div className="grid grid-cols-2 gap-2">
         {SHEET_SIDES.map((side) => (
           <Sheet key={side}>
-            <SheetTrigger>
+            <SheetTrigger asChild>
               <Button variant="outline">{side}</Button>
             </SheetTrigger>
             <SheetContent side={side}>
@@ -77,7 +212,7 @@ export function SheetSection() {
                 </div>
               </div>
               <SheetFooter>
-                <SheetClose>
+                <SheetClose asChild>
                   <Button type="submit">Save changes</Button>
                 </SheetClose>
               </SheetFooter>
@@ -85,6 +220,80 @@ export function SheetSection() {
           </Sheet>
         ))}
       </div>
+    );
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full items-center justify-center space-x-2">
+            <SheetSide />
+          </div>
+        }
+      />
+
+      <Pagination className="mt-10">
+        <PaginationContent className="flex w-full flex-row justify-between">
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.LABEL}>
+              <Button className="gap-1 pl-1" variant="outline">
+                <ChevronLeft />
+                Label
+              </Button>
+            </A>
+          </PaginationItem>
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.SELECT}>
+              <Button className="gap-1 pr-1 capitalize" variant="outline">
+                Select
+                <ChevronRight />
+              </Button>
+            </A>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+}
+
+const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
+
+type SheetSide = (typeof SHEET_SIDES)[number];
+
+export function SheetSide() {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      {SHEET_SIDES.map((side) => (
+        <Sheet key={side}>
+          <SheetTrigger asChild>
+            <Button variant="outline">{side}</Button>
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>Make changes to your profile here. Click save when you're done.</SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input id="username" value="@peduarte" className="col-span-3" />
+              </div>
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit">Save changes</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      ))}
     </div>
   );
 }

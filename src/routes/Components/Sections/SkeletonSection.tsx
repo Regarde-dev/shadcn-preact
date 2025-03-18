554350a871c6e9e66d11a312cb59f1b07c340acf
LiasCode <liasraul00@gmail.com>
2025-03-17 23:32:49 -0400

    feat(docs): skeleton guide

--- src/routes/Components/Sections/SkeletonSection.tsx
+++ src/routes/Components/Sections/SkeletonSection.tsx
@@ -1,8 +1,21 @@
+import { CodePreviewTabs } from "@/components/CodePreview/CodePreviewTabs";
+import HighlightCode from "@/components/CodePreview/HighlightCode";
+import { AppRoutes } from "@/routes/AppRoutes";
+import { Button } from "@ui/button";
+import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
 import { Skeleton } from "@ui/skeleton";
+import { ChevronLeft, ChevronRight } from "lucide-preact";
+import { A } from "preact-hashish-router";
 
 export function SkeletonSection() {
   return (
-    <div className="flex w-full flex-col items-center gap-10">
+    <div className="flex w-full flex-col gap-6">
+      <CodePreviewTabs
+        codeString={`
+  import { Skeleton } from "@ui/skeleton"
+
+  export function SkeletonDemo() {
+    return (
       <div className="flex items-center space-x-4">
         <Skeleton className="h-12 w-12 rounded-full" />
         <div className="space-y-2">
@@ -10,7 +23,52 @@ export function SkeletonSection() {
           <Skeleton className="h-4 w-[200px]" />
         </div>
       </div>
+    )
+  }
 
+
+`}
+        previewElement={
+          <div className="flex w-full items-center justify-center space-x-2">
+            <div className="flex items-center space-x-4">
+              <Skeleton className="h-12 w-12 rounded-full" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
+          </div>
+        }
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Usage</h2>
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  import { Skeleton } from "@ui/skeleton"
+
+`}
+      />
+
+      <HighlightCode
+        lang="tsx"
+        codeString={`
+  <Skeleton className="w-[100px] h-[20px] rounded-full" />
+
+`}
+      />
+
+      <h2 className="w-full border-b-2 pb-1 font-semibold text-2xl">Examples</h2>
+
+      <h3 className="w-full font-semibold text-xl">Card</h3>
+
+      <CodePreviewTabs
+        codeString={`
+  import { Skeleton } from "@ui/skeleton"
+
+  export function SkeletonCard() {
+    return (
       <div className="flex flex-col space-y-3">
         <Skeleton className="h-[125px] w-[250px] rounded-xl" />
         <div className="space-y-2">
@@ -18,6 +76,43 @@ export function SkeletonSection() {
           <Skeleton className="h-4 w-[200px]" />
         </div>
       </div>
+    )
+  }
+
+`}
+        previewElement={
+          <div className="flex w-full items-center justify-center space-x-2">
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
+          </div>
+        }
+      />
+
+      <Pagination className="mt-10">
+        <PaginationContent className="flex w-full flex-row justify-between">
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.SHEET}>
+              <Button className="gap-1 pl-1" variant="outline">
+                <ChevronLeft />
+                Sheet
+              </Button>
+            </A>
+          </PaginationItem>
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.SWITCH}>
+              <Button className="gap-1 pr-1 capitalize" variant="outline">
+                Switch
+                <ChevronRight />
+              </Button>
+            </A>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }

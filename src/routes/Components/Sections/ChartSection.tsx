9948f06469bd40381085ee2edf1833808749854f
LiasCode <liasraul00@gmail.com>
2025-03-18 01:45:36 -0400

    fix(docs): chart navigation

--- src/routes/Components/Sections/ChartSection.tsx
+++ src/routes/Components/Sections/ChartSection.tsx
@@ -1,13 +1,39 @@
+import { AppRoutes } from "@/routes/AppRoutes";
+import { Button } from "@ui/button";
+import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
+import { ChevronLeft, ChevronRight } from "lucide-preact";
+import { A } from "preact-hashish-router";
 import { AreaChartDemo1 } from "./Charts/AreaChartDemo1";
 import { BarChartDemo1 } from "./Charts/BarChartDemo1";
 import { PieChartDemo1 } from "./Charts/PieChartDemo1";
 
 export default function ChartSection() {
   return (
-    <div className="flex w-full flex-col gap-10 ">
+    <div className="flex w-full flex-col gap-6">
       <PieChartDemo1 />
       <BarChartDemo1 />
       <AreaChartDemo1 />
+
+      <Pagination className="mt-10">
+        <PaginationContent className="flex w-full flex-row justify-between">
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.CAROUSEL}>
+              <Button className="gap-1 pl-1" variant="outline">
+                <ChevronLeft />
+                Carousel
+              </Button>
+            </A>
+          </PaginationItem>
+          <PaginationItem>
+            <A href={AppRoutes.COMPONENTS.CHECKBOX}>
+              <Button className="gap-1 pr-1 capitalize" variant="outline">
+                Checkbox
+                <ChevronRight />
+              </Button>
+            </A>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }

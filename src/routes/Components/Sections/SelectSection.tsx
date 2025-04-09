516e4804974e4ba1816f0ceba51ab3cf425d58dc
LiasCode <liasraul00@gmail.com>
2025-04-09 05:18:59 -0400

    feat: basic select features

--- src/routes/Components/Sections/SelectSection.tsx
+++ src/routes/Components/Sections/SelectSection.tsx
@@ -2,7 +2,8 @@ import { AppRoutes } from "@/routes/AppRoutes";
 import { Alert, AlertDescription, AlertTitle } from "@ui/alert";
 import { Button } from "@ui/button";
 import { Pagination, PaginationContent, PaginationItem } from "@ui/pagination";
-import { AlertCircle, ChevronLeft, ChevronRight } from "lucide-preact";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@ui/select";
+import { AlertCircle, Apple, ChevronLeft, ChevronRight, Sun } from "lucide-preact";
 import { A } from "preact-hashish-router";
 
 export function SelectSection() {
@@ -13,6 +14,34 @@ export function SelectSection() {
         <AlertTitle>Page under construction</AlertTitle>
         <AlertDescription>Select component guide page is on work in progress.</AlertDescription>
       </Alert>
+
+      <Select defaultValue="blueberry">
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel className="flex flex-row items-center gap-2 [&_svg]:h-4 [&_svg]:w-4">
+              Fruits
+              <Apple />
+            </SelectLabel>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+
+          <SelectGroup>
+            <SelectLabel className="flex flex-row items-center gap-2 [&_svg]:h-4 [&_svg]:w-4">
+              Theme <Sun />
+            </SelectLabel>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
 
       <Pagination className="mt-10">
         <PaginationContent className="flex w-full flex-row justify-between">

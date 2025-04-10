118ff1b0481347f67fce861d3da315feeaba1615
Hansel Tapia <hanselt0620@gmail.com>
2025-04-09 23:45:50 -0400

    fix: changed way to get body scrollbar Y active for modals

--- src/components/ui/share/getScrollBarWidth.ts
+++ src/components/ui/share/getScrollBarWidth.ts
@@ -3,22 +3,7 @@ export function getScrollBarWidth(element: HTMLElement): number {
     throw new Error("A valid DOM element must be provided.");
   }
 
-  // Create a temporary element
-  const outer = document.createElement("div");
-  outer.style.visibility = "hidden";
-  outer.style.overflow = "scroll"; // Force the scrollbar to appear
-  outer.style.width = "100px";
-  outer.style.height = "100px";
-  document.body.appendChild(outer);
-
-  const inner = document.createElement("div");
-  inner.style.width = "100%";
-  inner.style.height = "100%";
-  outer.appendChild(inner);
-
-  const scrollBarWidth = outer.offsetWidth - inner.offsetWidth;
-
-  outer.parentNode?.removeChild(outer);
+  const scrollBarWidth = window.innerWidth - element.offsetWidth ;
 
   return scrollBarWidth;
 }

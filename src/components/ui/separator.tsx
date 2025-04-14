d28a5baa243ceb01b66d9d210e2b5155aa1cd0b0
LiasCode <liasraul00@gmail.com>
2025-04-13 23:51:50 -0400

    feat: add Separator component with orientation and decorative props

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,57 @@
+import { type HTMLAttributes, forwardRef } from "preact/compat";
+import { cn } from "./share/cn";
+
+const DEFAULT_ORIENTATION = "horizontal";
+
+const ORIENTATIONS = ["horizontal", "vertical"] as const;
+
+type Orientation = (typeof ORIENTATIONS)[number];
+
+export type SeparatorProps = HTMLAttributes<HTMLDivElement> & {
+  /**
+   * Either `vertical` or `horizontal`. Defaults to `horizontal`.
+   */
+  orientation?: Orientation;
+  /**
+   * Whether or not the component is purely decorative. When true, accessibility-related attributes
+   * are updated so that that the rendered element is removed from the accessibility tree.
+   */
+  decorative?: boolean;
+};
+
+export const Separator = forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, class: classNative, ...props }, forwardedRef) => {
+    const { decorative, orientation: orientationProp = DEFAULT_ORIENTATION, ...domProps } = props;
+
+    const orientation = isValidOrientation(orientationProp) ? orientationProp : DEFAULT_ORIENTATION;
+
+    // `aria-orientation` defaults to `horizontal` so we only need it if `orientation` is vertical
+    const ariaOrientation = orientation === "vertical" ? orientation : undefined;
+
+    const semanticProps = decorative ? { role: "none" } : { "aria-orientation": ariaOrientation, role: "separator" };
+
+    return (
+      // @ts-expect-error
+      <div
+        data-orientation={orientation}
+        {...semanticProps}
+        {...domProps}
+        ref={forwardedRef}
+        className={cn(
+          "shrink-0 bg-border",
+          orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+          className,
+          classNative
+        )}
+        {...props}
+      />
+    );
+  }
+);
+Separator.displayName = "Separator";
+
+/* -----------------------------------------------------------------------------------------------*/
+
+function isValidOrientation(orientation: any): orientation is Orientation {
+  return ORIENTATIONS.includes(orientation);
+}

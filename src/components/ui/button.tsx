2c41cf421b1482e844d28711553dba07b202727a
LiasCode <liasraul00@gmail.com>
2025-04-14 02:45:23 -0400

    feat: add basic slot and compose refs methods

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,8 @@
 import { type VariantProps, cva } from "class-variance-authority";
 import { type ButtonHTMLAttributes, forwardRef } from "preact/compat";
 import { cn } from "./share/cn";
+import { composeRefs } from "./share/compose_ref";
+import { Slot } from "./share/slot";
 
 export const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 gap-2 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -34,7 +36,11 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, Va
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, class: classNative, variant, size, asChild, ...props }, ref) => {
-    return <button className={cn(buttonVariants({ variant, size, className }), classNative)} ref={ref} {...props} />;
+    const Comp = asChild ? Slot : "button";
+
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }), classNative)} ref={ref as any} {...props} />
+    );
   }
 );
 Button.displayName = "Button";

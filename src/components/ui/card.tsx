6d00b4559a3ff7515ff950a83dff2cf6f190649a
LiasCode <liasraul00@gmail.com>
2025-05-12 19:30:18 -0400

    fix: card rename ref as forwardedRef

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,21 +3,23 @@ import { cn } from "./share/cn";
 
 export type CardProps = HTMLAttributes<HTMLDivElement>;
 
-export const Card = forwardRef<HTMLDivElement, CardProps>(({ className, class: classNative, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("rounded-xl border bg-card text-card-foreground shadow", className, classNative)}
-    {...props}
-  />
-));
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ className, class: classNative, ...props }, forwardedRef) => (
+    <div
+      ref={forwardedRef}
+      className={cn("rounded-xl border bg-card text-card-foreground shadow", className, classNative)}
+      {...props}
+    />
+  )
+);
 Card.displayName = "Card";
 
 export type CardHeaderProps = HTMLAttributes<HTMLDivElement>;
 
 export const CardHeader = forwardRef<HTMLDivElement, CardHeaderProps>(
-  ({ className, class: classNative, ...props }, ref) => (
+  ({ className, class: classNative, ...props }, forwardedRef) => (
     <div
-      ref={ref}
+      ref={forwardedRef}
       className={cn("flex flex-col space-y-1.5 p-6", className, classNative)}
       {...props}
     />
@@ -28,9 +30,9 @@ CardHeader.displayName = "CardHeader";
 export type CardTitleProps = HTMLAttributes<HTMLDivElement>;
 
 export const CardTitle = forwardRef<HTMLDivElement, CardTitleProps>(
-  ({ className, class: classNative, ...props }, ref) => (
+  ({ className, class: classNative, ...props }, forwardedRef) => (
     <div
-      ref={ref}
+      ref={forwardedRef}
       className={cn("font-semibold leading-none tracking-tight", className, classNative)}
       {...props}
     />
@@ -41,9 +43,9 @@ CardTitle.displayName = "CardTitle";
 export type CardDescriptionProps = HTMLAttributes<HTMLDivElement>;
 
 export const CardDescription = forwardRef<HTMLDivElement, CardDescriptionProps>(
-  ({ className, class: classNative, ...props }, ref) => (
+  ({ className, class: classNative, ...props }, forwardedRef) => (
     <div
-      ref={ref}
+      ref={forwardedRef}
       className={cn("text-muted-foreground text-sm", className, classNative)}
       {...props}
     />
@@ -54,9 +56,9 @@ CardDescription.displayName = "CardDescription";
 export type CardContentProps = HTMLAttributes<HTMLDivElement>;
 
 export const CardContent = forwardRef<HTMLDivElement, CardContentProps>(
-  ({ className, class: classNative, ...props }, ref) => (
+  ({ className, class: classNative, ...props }, forwardedRef) => (
     <div
-      ref={ref}
+      ref={forwardedRef}
       className={cn("p-6 pt-0", className, classNative)}
       {...props}
     />
@@ -67,9 +69,9 @@ CardContent.displayName = "CardContent";
 export type CardFooterProps = HTMLAttributes<HTMLDivElement>;
 
 export const CardFooter = forwardRef<HTMLDivElement, CardFooterProps>(
-  ({ className, class: classNative, ...props }, ref) => (
+  ({ className, class: classNative, ...props }, forwardedRef) => (
     <div
-      ref={ref}
+      ref={forwardedRef}
       className={cn("flex items-center p-6 pt-0", className, classNative)}
       {...props}
     />
